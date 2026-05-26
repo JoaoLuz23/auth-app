b938a00072bd3f3a8a87808011b3197ae9e7f1a9
JoaoLuz23 <jpferreiraluz23@gmail.com>
2026-05-26 14:14:17 -0300

    Preparando Repositorio

---
+++ resources/views/categorias/index.blade.php
@@ -0,0 +1,41 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Categorias') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                     <a href="{{ route('categorias.create') }}"> ➕Adicione uma nova categoria</a> 
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($categorias as $categoria)
+                                <tr>
+                                    <td>{{ $categoria->name }}</td>
+                                    <td>
+                                       
+                                        <a href="{{ route('categorias.edit', $categoria) }}" class="text-indigo-600 hover:text-indigo-900"> ♻️   Edit</a>
+                                        <form action="{{ route('categorias.destroy', $categoria) }}" method="POST" style="display: inline;">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="text-red-600 hover:text-red-900"> ❌   Delete</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
